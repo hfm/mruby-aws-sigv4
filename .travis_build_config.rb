3e97e01b474dbe2e104a128c7351e9bc0010793e
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-03-11 01:59:40 +0900

    add test gems

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,6 +1,11 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
-  conf.gem File.expand_path(File.dirname(__FILE__))
   conf.enable_test
+
+  conf.gem File.expand_path(File.dirname(__FILE__))
+  conf.gem mgem: 'mruby-dir'
+  conf.gem mgem: 'mruby-dir-glob'
+  conf.gem mgem: 'mruby-io'
+  conf.gem mgem: 'mruby-regexp-pcre'
 end
